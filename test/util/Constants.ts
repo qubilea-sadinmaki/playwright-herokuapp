659f89c126954c7cf8345606513e1da529e69166
Petri Sadinmäki <60505390+qubilea-sadinmaki@users.noreply.github.com>
2024-08-23 11:35:29 +0300

    new .env file but its not committed

--- test/util/Constants.ts
+++ test/util/Constants.ts
@@ -2,9 +2,10 @@ import { User } from "./types";
 require('dotenv').config()
 
 export class Constants {
+
     static readonly BASE_URL: string = 'https://thinking-tester-contact-list.herokuapp.com';
     static readonly MAIN_USER: User = {
-        username: `${process.env.USER_NAME}`,
+        username: `${process.env.USERNAME}`,
         password: `${process.env.PASSWORD}`
     };
 }
